ab8a899886d71f05ff8bda0d64b087b3e8bd8540
changxiangyu <1792286994@qq.com>
2019-10-23 11:07:06 +0800

    vue 中常用指令

--- 演示DEMO源码/src/views/1.7/Directives.vue
+++ 演示DEMO源码/src/views/1.7/Directives.vue
@@ -14,14 +14,19 @@
     <div v-else-if="number === 2">hello world {{ number }}</div>
     <div v-else>hello geektime {{ number }}</div>
     <h2>v-for v-bind</h2>
+    <!-- v-bind 绑定后面的变量或表达式 简写：冒号（：） -->
     <div v-for="num in [1, 2, 3]" v-bind:key="num">hello vue {{ num }}</div>
     <h2>v-on</h2>
+    <!-- v-on 事件 简写：@ -->
     <button v-on:click="number = number + 1">number++</button>
     <h2>v-model</h2>
+    <!-- v-model 双向绑定语法糖的一个指令 -->
     <input v-model="message" />
     <h2>v-pre</h2>
+    <!-- v-pre 绕过{{}}语法的编译过程，直接输出字符串 -->
     <div v-pre>{{ this will not be compiled }}</div>
     <h2>v-once</h2>
+    <!-- v-once {{}}语法解析的变量只会执行一次 -->
     <div v-once>
       {{ number }}
     </div>
